54ce678797a168e0b46abe779ba0d4268f7d0d38
Foqz <fikskamil@gmail.com>
2020-06-09 09:58:39 +0200

    Added customer update option

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,10 +5,7 @@ import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,10 +18,8 @@ public class CustomerController {
 
     @GetMapping("/list")
     public String listCustomers(Model model) {
-
         List<Customer> customers = customerService.getCustomers();
         model.addAttribute("customers", customers);
-
         return "list-customers";
     }
 
@@ -37,9 +32,13 @@ public class CustomerController {
 
     @PostMapping("saveCustomer")
     public String saveCustomer(@ModelAttribute("customer") Customer customer) {
-
         customerService.saveCustomer(customer);
-
         return "redirect:/customer/list";
+    }
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("customerId")int id, Model model) {
+        Customer customer = customerService.getCustomer(id);
+        model.addAttribute("customer", customer);
+        return "customer-form";
     }
 }
